500ae2bbd3bca3524a1d4bed8d440153c101bc47
markskwid <bascal.markponce.f.1015@gmail.com>
2022-06-11 15:18:03 +0800

    made changes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const User = require("../routes/userRoutes");
 const Logs = require("../routes/logsRoutes");
 const connection = require("../database/connection.js");
 var fs = require("fs");
+const publicPath = path.join(__dirname, "..", "client");
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
@@ -20,17 +21,8 @@ app.use("/api", fileRouter);
 app.use("/manage-user", User);
 app.use("/logs", Logs);
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/public")));
-
-// Handle GET requests to /api route
-app.get("/api-server", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-// All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
+  res.sendFile("../client/public/index.html");
 });
 
 app.use("/public", express.static(path.join(__dirname + "/public")));
