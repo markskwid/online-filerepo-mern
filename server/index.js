26c4079792742d985d651c046f569393ca95fb9a
markskwid <bascal.markponce.f.1015@gmail.com>
2022-06-11 15:10:06 +0800

    made changes

--- server/index.js
+++ server/index.js
@@ -20,6 +20,19 @@ app.use("/api", fileRouter);
 app.use("/manage-user", User);
 app.use("/logs", Logs);
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/public")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
+});
+
 app.use("/public", express.static(path.join(__dirname + "/public")));
 
 app.use(function (req, res, next) {
